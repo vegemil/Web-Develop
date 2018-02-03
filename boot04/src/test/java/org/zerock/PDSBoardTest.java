058831d2aa10156072139bcb07b3665ba6c3ac51
으나 :) <euna501@gmail.com>
2018-02-03 09:53:42 +0900

    JPA를 사용해서 Table Join해보기

---
+++ boot04/src/test/java/org/zerock/PDSBoardTest.java
@@ -0,0 +1,40 @@
+package org.zerock;
+
+import java.util.Arrays;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.zerock.domain.PDSBoard;
+import org.zerock.domain.PDSFile;
+import org.zerock.persistence.PDSBoardRepository;
+
+import lombok.extern.java.Log;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Log
+public class PDSBoardTest {
+	@Autowired
+	PDSBoardRepository repo;
+	
+	@Test
+	public void testInsertPDS() {
+		PDSBoard pds = new PDSBoard();
+		pds.setPname("DOCUMENT 1 - 2");
+		
+		PDSFile file1 = new PDSFile();
+		file1.setPdsfile("file1.doc");
+		
+		PDSFile file2 = new PDSFile();
+		file2.setPdsfile("file2.doc");
+		
+		pds.setFiles(Arrays.asList(file1, file2));
+		
+		log.info("try to save pds");
+		
+		repo.save(pds);
+	}
+}
