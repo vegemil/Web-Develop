b88132a838366d982583391c768765ede13a14dc
으나 :) <euna501@gmail.com>
2018-02-10 18:41:04 +0900

    querydsl사용해서 페이징 하기

--- boot06/src/main/java/org/zerock/persistence/WebBoardRepository.java
+++ boot06/src/main/java/org/zerock/persistence/WebBoardRepository.java
@@ -2,8 +2,20 @@ package org.zerock.persistence;
 
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.data.repository.CrudRepository;
+import org.zerock.domain.QWebBoard;
 import org.zerock.domain.WebBoard;
+
+import com.querydsl.core.BooleanBuilder;
+import com.querydsl.core.types.Predicate;
 
 public interface WebBoardRepository extends CrudRepository<WebBoard, Long>, QuerydslPredicateExecutor<WebBoard>{
 
+	public default Predicate makePredicate(String type, String keyword) {
+		BooleanBuilder builder = new BooleanBuilder();
+		QWebBoard board = QWebBoard.webBoard;
+		
+		builder.and(board.bno.gt(0));
+		
+		return builder;
+	}
 }
--- boot06/src/test/java/org/zerock/WebBoardRepositoryTests.java
+++ boot06/src/test/java/org/zerock/WebBoardRepositoryTests.java
@@ -8,6 +8,10 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.test.annotation.Commit;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.zerock.domain.WebBoard;
@@ -23,16 +27,28 @@ public class WebBoardRepositoryTests {
 	@Autowired
 	WebBoardRepository repo;
 	
+//	@Test
+//	public void insertBoardDummies() {
+//		IntStream.range(0, 300).forEach(i -> {
+//			WebBoard board = new WebBoard();
+//			
+//			board.setTitle("Sample Board Title " + i);
+//			board.setContent("Content Sample ... " + i + " of Board ");
+//			board.setWriter("user0" + (i % 10));
+//			
+//			repo.save(board);
+//		});
+//	}
+	
 	@Test
-	public void insertBoardDummies() {
-		IntStream.range(0, 300).forEach(i -> {
-			WebBoard board = new WebBoard();
-			
-			board.setTitle("Sample Board Title " + i);
-			board.setContent("Content Sample ... " + i + " of Board ");
-			board.setWriter("user0" + (i % 10));
-			
-			repo.save(board);
-		});
+	public void testList1() {
+		Pageable pageable = PageRequest.of(0, 20, Direction.DESC, "bno");
+		
+		Page<WebBoard> result = repo.findAll(repo.makePredicate(null, null), pageable);
+		
+		log.info("PAGE " + result.getPageable());
+		
+		log.info("---------------------------");
+		result.getContent().forEach(board -> log.info("" + board));
 	}
 }
