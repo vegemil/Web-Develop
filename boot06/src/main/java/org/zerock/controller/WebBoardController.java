ff947a364c35685b10d06177f74e6852261984fa
으나 :) <euna501@gmail.com>
2018-02-21 22:54:45 +0900

    PageVO로 페이징 하기

--- boot06/src/main/java/org/zerock/controller/WebBoardController.java
+++ boot06/src/main/java/org/zerock/controller/WebBoardController.java
@@ -1,8 +1,10 @@
 package org.zerock.controller;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.zerock.vo.PageVO;
 
 import lombok.extern.java.Log;
 
@@ -12,7 +14,8 @@ import lombok.extern.java.Log;
 public class WebBoardController{
 	
 	@GetMapping("/list")
-	public void list() {
-		log.info("list() called...");
+	public void list(PageVO vo) {
+		Pageable page = vo.makePageable(0, "bno");
+		log.info("list() called..." +page);
 	}
 }
