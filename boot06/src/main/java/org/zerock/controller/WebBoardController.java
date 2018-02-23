25b9db3fa67f17996ee6d47a03078788e0854673
으나 :) <euna501@gmail.com>
2018-02-24 01:20:27 +0900

    테이블에 데이터 넣고 페이지 번호 넣어 이동시킴

--- boot06/src/main/java/org/zerock/controller/WebBoardController.java
+++ boot06/src/main/java/org/zerock/controller/WebBoardController.java
@@ -1,21 +1,38 @@
 package org.zerock.controller;
 
-import org.springframework.data.domain.Pageable;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.zerock.vo.PageVO;
+
 
 import lombok.extern.java.Log;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.zerock.domain.WebBoard;
+import org.zerock.persistence.WebBoardRepository;
+import org.zerock.vo.PageMaker;
+import org.zerock.vo.PageVO;
 
 @Controller
 @RequestMapping("/boards/")
 @Log
 public class WebBoardController{
 	
+	@Autowired
+	private WebBoardRepository repo;
+
 	@GetMapping("/list")
-	public void list(PageVO vo) {
+	public void list(PageVO vo, Model model) {
 		Pageable page = vo.makePageable(0, "bno");
-		log.info("list() called..." +page);
+		Page<WebBoard> result = repo.findAll(repo.makePredicate(null, null), page);
+		
+		log.info("" + page);
+		log.info("" + result);
+		
+		log.info("TOTAL PAGE NUMBER : " + result.getTotalPages());
+		
+		model.addAttribute("result", new PageMaker(result));
 	}
 }
