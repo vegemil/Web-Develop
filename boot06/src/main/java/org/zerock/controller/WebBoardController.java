2e870affdfc54d59b52592413c21422d66ca1f2b
으나 :) <euna501@gmail.com>
2018-02-24 15:52:39 +0900

    게시판에 글 추가 기능 추가

--- boot06/src/main/java/org/zerock/controller/WebBoardController.java
+++ boot06/src/main/java/org/zerock/controller/WebBoardController.java
@@ -10,7 +10,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.zerock.domain.WebBoard;
 import org.zerock.persistence.WebBoardRepository;
 import org.zerock.vo.PageMaker;
@@ -33,8 +35,24 @@ public class WebBoardController{
 //		log.info("" + page);
 //		log.info("" + result);
 		
-		log.info("TOTAL PAGE NUMBER : " + result.getTotalPages());
+//		log.info("TOTAL PAGE NUMBER : " + result.getTotalPages());
 		
 		model.addAttribute("result", new PageMaker(result));
+	}
+
+	@GetMapping("/register")
+	public void registerGET(@ModelAttribute("vo") WebBoard vo) {
+		log.info("register get");
+	}
+
+	@PostMapping("/register")
+	public String registerPOST(@ModelAttribute("vo") WebBoard vo, RedirectAttributes rttr) {
+		log.info("register post");
+		log.info("" + vo);
+
+		repo.save(vo);
+		rttr.addFlashAttribute("msg", "success");
+
+		return "redirect:/boards/list";
 	}
 }
