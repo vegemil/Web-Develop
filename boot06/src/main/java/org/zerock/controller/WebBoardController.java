c74f5d3c0ab675350418dbbb9f4e9e653080c9ca
으나 :) <euna501@gmail.com>
2018-03-15 00:35:30 +0900

    게시판 수정, 삭제 기능 추가

--- boot06/src/main/java/org/zerock/controller/WebBoardController.java
+++ boot06/src/main/java/org/zerock/controller/WebBoardController.java
@@ -55,4 +55,54 @@ public class WebBoardController{
 
 		return "redirect:/boards/list";
 	}
+
+	@GetMapping("/view")
+	public void view(Long bno, @ModelAttribute("pageVO") PageVO vo, Model model) {
+		log.info("BNO : " + bno);
+		repo.findById(bno).ifPresent(board -> model.addAttribute("vo", board));
+	}
+
+	@GetMapping("/modify")
+	public void modify(Long bno, @ModelAttribute("pageVO") PageVO vo, Model model) {
+		log.info("MODIFY BNO : " + bno);
+		repo.findById(bno).ifPresent(board -> model.addAttribute("vo", board));
+	}
+	
+	@PostMapping("/delete")
+	public String delete(Long bno, PageVO vo, RedirectAttributes rttr) {
+		log.info("DELETE BNO : " + bno);
+		repo.deleteById(bno);
+
+		rttr.addFlashAttribute("msg", "success");
+
+		//페이징과 검색했던 결과로 이동하는 경우
+		rttr.addAttribute("page", vo.getPage());
+		rttr.addAttribute("size", vo.getSize());
+		rttr.addAttribute("type", vo.getType());
+		rttr.addAttribute("keyword", vo.getKeyword());
+
+		return "redirect:/boards/list";
+	}
+
+	@PostMapping("/modify")
+	public String modifyPost(WebBoard board, PageVO vo, RedirectAttributes rttr) {
+		log.info("Modify WebBoard : " + board);
+
+		repo.findById(board.getBno()).ifPresent(origin -> {
+			origin.setTitle(board.getTitle());
+			origin.setContent(board.getContent());
+
+			repo.save(origin);
+			rttr.addFlashAttribute("msg", "success");
+			rttr.addAttribute("bno", origin.getBno());
+		});
+
+		//페이징과 검색했던 결과로 이동하는 경우
+		rttr.addAttribute("page", vo.getPage());
+		rttr.addAttribute("size", vo.getSize());
+		rttr.addAttribute("type", vo.getType());
+		rttr.addAttribute("keyword", vo.getKeyword());
+		
+		return "redirect:/boards/view";
+	}
 }
